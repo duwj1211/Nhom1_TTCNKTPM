f7c330099bcd190330709ad76c10d887fad9c90e
nguyentuan <dungtuann67@gmail.com>
2024-04-08 20:24:56 +0700

    fix some bug

--- FrontEnd/src/pages/Cart/Cart.js
+++ FrontEnd/src/pages/Cart/Cart.js
@@ -46,7 +46,7 @@ export default function Cart(){
                                         <a><span className="ahfb-svg-iconset ast-inline-flex"><svg className="ast-mobile-svg ast-close-svg" fill="currentColor" version="1.1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M5.293 6.707l5.293 5.293-5.293 5.293c-0.391 0.391-0.391 1.024 0 1.414s1.024 0.391 1.414 0l5.293-5.293 5.293 5.293c0.391 0.391 1.024 0.391 1.414 0s0.391-1.024 0-1.414l-5.293-5.293 5.293-5.293c0.391-0.391 0.391-1.024 0-1.414s-1.024-0.391-1.414 0l-5.293 5.293-5.293-5.293c-0.391-0.391-1.024-0.391-1.414 0s-0.391 1.024 0 1.414z"></path></svg></span></a>
                                     </td>
                                     <td className={cx("product-thumbnail")}>
-                                        <a><img fetchpriority="high" decoding="async" width="100" height="150" src="https://websitedemos.net/book-store-02/wp-content/uploads/sites/834/2021/05/author-book-store-book-cover-07-300x450.jpg" srcset="https://websitedemos.net/book-store-02/wp-content/uploads/sites/834/2021/05/author-book-store-book-cover-07-300x450.jpg 300w, https://websitedemos.net/book-store-02/wp-content/uploads/sites/834/2021/05/author-book-store-book-cover-07-200x300.jpg 200w, https://websitedemos.net/book-store-02/wp-content/uploads/sites/834/2021/05/author-book-store-book-cover-07.jpg 400w" sizes="(max-width: 300px) 100vw, 300px"></img></a>						
+                                        <a><img fetchpriority="high" decoding="async" width="100" height="150" src="https://websitedemos.net/book-store-02/wp-content/uploads/sites/834/2021/05/author-book-store-book-cover-07-300x450.jpg" srcSet="https://websitedemos.net/book-store-02/wp-content/uploads/sites/834/2021/05/author-book-store-book-cover-07-300x450.jpg 300w, https://websitedemos.net/book-store-02/wp-content/uploads/sites/834/2021/05/author-book-store-book-cover-07-200x300.jpg 200w, https://websitedemos.net/book-store-02/wp-content/uploads/sites/834/2021/05/author-book-store-book-cover-07.jpg 400w" sizes="(max-width: 300px) 100vw, 300px"></img></a>						
                                     </td>
                                     <td className={cx("product-name")}data-title="Product">
                                         <a>The Throned Mirror</a>						
@@ -56,7 +56,7 @@ export default function Cart(){
                                     </td>
                                     <td className={cx("product-quantity")} data-title="Quantity">
                                         <div >
-                                            <input className={cx("quantity-input")} type="number" id="quantity_660abf30afbb2" name="cart[58ae749f25eded36f486bc85feb3f0ab][qty]"  aria-label="Product quantity" size="4" min="0" max="" step="1" placeholder="" inputmode="numeric" autoComplete="on"></input>
+                                            <input className={cx("quantity-input")} type="number" id="quantity_660abf30afbb2" name="cart[58ae749f25eded36f486bc85feb3f0ab][qty]"  aria-label="Product quantity" size="4" min="0" max="" step="1" placeholder="" inputMode="numeric" autoComplete="on"></input>
                                         </div>
                                     </td>
                                     <td className={cx("product-subtotal")} data-title="Subtotal">
@@ -64,11 +64,11 @@ export default function Cart(){
                                     </td>
                                 </tr>
                                 <tr>
-                                    <td colspan="6">
+                                    <td colSpan="6">
                                         <div className={cx("coupon")}>
                                             <div className="form-floating mb-1">
                                                 <input type="email" className={cx("coupon-input","form-control")} id="floatingInput" placeholder="Coupon code"></input>
-                                                <label for="floatingInput">Mã giảm giá</label>
+                                                <label htmlFor="floatingInput">Mã giảm giá</label>
                                             </div>
                                             <button type="button" className={cx("apply-btn","btn","btn-outline-custom")}>Áp dụng </button>
                                             <button type="button" className={cx("update-btn","btn","btn-outline-custom")} disabled = "">Cập nhật</button>                                     
@@ -84,7 +84,7 @@ export default function Cart(){
                     <table className={cx('cart-total-table','table')}>
                         <thead>
                             <tr>
-                                <th colspan="2"><h2>Tổng số trong giỏ hàng</h2></th>
+                                <th colSpan="2"><h2>Tổng số trong giỏ hàng</h2></th>
                             </tr>
                         </thead>
                         <tbody>
@@ -97,7 +97,7 @@ export default function Cart(){
                                 <td><span><bdi><span>$</span>23.00</bdi></span></td>
                             </tr>
                             <tr>
-                                <td colspan="2">
+                                <td colSpan="2">
                                     <div className={cx("process-checkout")}><button type='button' className={cx("checkout-btn","btn","btn-outline-custom")} onClick={handleButtonClick}>Tiến hành thanh toán</button></div>
                                 </td>
                             </tr>
