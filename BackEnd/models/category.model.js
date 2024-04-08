fec3c1efd72bd04fb502cabe62bbcc527b8b0f57
nguyentuan <dungtuann67@gmail.com>
2024-04-08 21:18:10 +0700

    update category

--- BackEnd/models/category.model.js
+++ BackEnd/models/category.model.js
@@ -4,7 +4,6 @@ const slug = require('mongoose-slug-updater');
 const CategorySchema = mongoose.Schema({
   name: { type: String, require: true },
   slug: {type: String, slug: 'name'},
-  books: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Book' }]
 })
 
 const Category = mongoose.model("Category", CategorySchema);
