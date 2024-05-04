a14895717d829a54d561fcaa2da0f78795f4b153
nguyenkhoa2308 <nguyenduckhoa.ndk.2308@gmail.com>
2024-05-04 18:46:11 +0700

    fix wont load image footer

--- FrontEnd/src/layouts/components/Footer/index.js
+++ FrontEnd/src/layouts/components/Footer/index.js
@@ -1,6 +1,14 @@
 import classNames from 'classnames/bind';
 import styles from './Footer.module.css';
 import { Link } from 'react-router-dom';
+import logo from '../../../assets/images/img_logo_1.png';
+import facebook from '../../../assets/images/img_facebook_logo.png';
+import linkedin from '../../../assets/images/img_linkedin_logo.png';
+import twitter from '../../../assets/images/img_twitter_logo.png';
+import youtube from '../../../assets/images/img_youtube_logo.png';
+import thumb1 from '../../../assets/images/img_rectangle_22_100x100.png';
+import thumb2 from '../../../assets/images/img_rectangle_22.png';
+import unsplash from '../../../assets/images/img_unsplash.png';
 
 const cx = classNames.bind(styles)
 
@@ -11,21 +19,21 @@ function Footer() {
                 <section className={cx('index-module_row')}>
                     <section className={cx('index-module_col', 'index-module_l-4')}>
                         <Link to={'/'} className={cx('logo')}>
-                            <img className={cx('top-logo')} src="./images/img_logo_1.png" alt="image logo"/>
+                            <img className={cx('top-logo')} src={logo} alt="image logo"/>
                         </Link>
                         <p>Nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                         <section className={cx('index-module_row', 'social-list')}>
                                 <Link to={'/'} className={cx('social-item')} title='1Book trên Facebook' target='_blank' rel='noreferrer'>
-                                    <img className={cx('social-logo')} src="./images/img_facebook_logo.png" alt="facebook logo"/>
+                                    <img className={cx('social-logo')} src={facebook} alt="facebook logo"/>
                                 </Link>
                                 <Link to={'/'} className={cx('social-item')} title='1Book trên Linkedin' target='_blank' rel='noreferrer'>
-                                    <img className={cx('social-logo')} src="./images/img_linkedin_logo.png" alt="linkedin logo"/>
+                                    <img className={cx('social-logo')} src={linkedin} alt="linkedin logo"/>
                                 </Link>
                                 <Link to={'/'} className={cx('social-item')} title='1Book trên Twitter' target='_blank' rel='noreferrer'>
-                                    <img className={cx('social-logo')} src="./images/img_twitter_logo.png" alt="twitter logo"/>
+                                    <img className={cx('social-logo')} src={twitter} alt="twitter logo"/>
                                 </Link>
                                 <Link to={'/'} className={cx('social-item')} title='1Book trên Youtube' target='_blank' rel='noreferrer'>
-                                    <img className={cx('social-logo')} src="./images/img_youtube_logo.png" alt="youtube logo"/>
+                                    <img className={cx('social-logo')} src={youtube} alt="youtube logo"/>
                                 </Link>  
                         </section>
                     </section>
@@ -67,7 +75,7 @@ function Footer() {
                                     <li className={cx('news-items')}>  
                                         <div className={cx('news-thumb')}>
                                             <Link to={'/'} target='_self' className={cx('link')}>
-                                                <img className={cx('image')} src="./images/img_rectangle_22.png" alt="image" />
+                                                <img className={cx('image')} src={thumb2} alt="image" />
                                             </Link>
                                         </div> 
                                         <div className={cx('news-container')}>
@@ -83,7 +91,7 @@ function Footer() {
                                     <li className={cx('news-items')}>
                                         <div className={cx('news-thumb')}>
                                             <Link to={'/'} target='_self'>
-                                                <img className={cx('image')} src="./images/img_rectangle_22_100x100.png" alt="image" />
+                                                <img className={cx('image')} src={thumb1} alt="image" />
                                             </Link>
                                         </div> 
                                         
