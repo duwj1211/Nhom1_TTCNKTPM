1333ad7444bb21eba3df3618d502b6165aab830e
nguyentuan <dungtuann67@gmail.com>
2024-05-09 22:55:24 +0700

    update css

--- FrontEnd/src/pages/ResetPassword/ResetPassword.js
+++ FrontEnd/src/pages/ResetPassword/ResetPassword.js
@@ -132,7 +132,7 @@ const ResetPassword = () => {
     return(
         <div className={cx("wrapper")}>
             <div className={cx("inner")}>
-                <img className={cx('image-holder')} src="https://colorlib.com/etc/regform/colorlib-regform-17/images/registration-form-1.jpg" alt="" />
+                <img className={cx('image-holder')} src={require("../../assets/images/banner-login.jpg")} alt="" />
                 <form>
                     <h3>Đặt lại mật khẩu</h3>
                     <h6>
