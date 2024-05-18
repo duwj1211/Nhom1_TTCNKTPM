9866b635a3b33b06cf047418460b1f61fd634cba
nguyenkhoa2308 <nguyenduckhoa.ndk.2308@gmail.com>
2024-05-18 18:22:31 +0700

    Author Detail Page

---
+++ FrontEnd/src/pages/BookDetails/BookInfo.js
@@ -0,0 +1,135 @@
+import classNames from 'classnames/bind';
+import styles from './BookInfo.module.css';
+import React, { useEffect, useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Link } from 'react-router-dom';
+import ApiService from "../../service/api.service";
+
+const cx = classNames.bind(styles);
+
+function BookInfo({bookInfo}) {
+    const [book, setBook] = useState(null);
+    const [quantity, setQuantity] = useState(1);
+
+    useEffect(() => {
+        setBook(bookInfo);
+    }, [bookInfo]);
+
+    const limitWord = (description, maxLength) => {
+        const data = description.split(' ');
+        if (data.length <= maxLength) {
+          return data.slice(0, data.length).join(' ');
+        }
+        return data.slice(0, maxLength).join(' ') + '...';
+    };
+    const handleChange = (event, maxQuantity) => {
+        let value = event.target.value;
+        if(value > maxQuantity){
+            event.target.value = maxQuantity;
+            value = maxQuantity
+        }
+        else if(!value){
+            value = 1;
+            event.target.value = 1;
+        }
+        setQuantity(value);
+    };
+
+    const handleAddToCart = async (bookId, quantity) => {
+        try{
+            const response = await ApiService.post('carts/add', {
+                bookId,
+                quantity
+            })
+            if(response.status === 200){
+                toast.success('Thêm vào giỏ hàng thành công!', {
+                    position: "top-right",
+                    autoClose: 3000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: false,
+                    draggable: false,
+                    progress: undefined,
+                    theme: "light",
+                });
+                console.log("Successful");
+            }else{
+                toast.error('Thêm vào giỏ hàng thất bại!', {
+                    position: "top-right",
+                    autoClose: 3000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                });
+                console.log("Unsuccessful");
+            }
+        }catch(error){
+            toast.error('Thêm vào giỏ hàng thất bại!', {
+                position: "top-right",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+            });
+            console.error('Error add to cart:',error);
+        }
+    }
+
+    return (
+        (book && <div className={cx("details")}>
+            <div className={cx("big-img")}>
+                <img src={book.avatar} alt="Book img"></img>
+            </div>
+            <div className={cx("grid")}>
+                <div className={cx("book-info")}>
+                    <h4 className={cx("book-title")}>{book.name}</h4>
+                    <div className={cx("book-price")}>
+                        <p className={cx("book-final-price")}>{(book.priceFinal / 1000).toFixed(3)} VNĐ</p>
+                        <p className={cx("book-original-price")}>{(book.priceOriginal / 1000).toFixed(3)} VNĐ</p>
+                    </div>
+                    <div>
+                        <div className={cx("fw-medium mb-2")}>Tác giả: {book.author.fullName}</div>
+                        <div className={cx("fw-medium mb-2")}>Dịch giả: {book.translator}</div>
+                        <div className={cx("fw-medium mb-2")}>Nhà xuất bản: {book.publisher}</div>
+                    </div>
+                    <p className={cx("book-desc")}>
+                        {limitWord(book.description, 50)}
+                        <Link className={cx("read-more-desc")} href="#description-tabs">
+                            Read More
+                        </Link>
+                    </p>
+                    <div className={cx("remain-quantity")}>
+                        <span>Còn hàng:&nbsp;</span>
+                        <span>{book.quantity} sản phẩm</span>
+                    </div>
+                    <div className={cx("cart")}>
+                        <input className={cx("quantity")} type="number" id="quantity" aria-label="Product quantity" size="4" min="1" max={book.quantity} step="1" placeholder="" inputMode="numeric" autoComplete="on" defaultValue="1" onChange={(e) => handleChange(e, book.quantity)}></input>
+                        <button type="button" className={cx("btn")} onClick={() => handleAddToCart(book._id, quantity)}>
+                            Add to cart
+                        </button>
+                        <ToastContainer position="top-right"
+                            autoClose={5000}
+                            hideProgressBar={false}
+                            newestOnTop={false}
+                            closeOnClick
+                            rtl={false}
+                            pauseOnFocusLoss={false}
+                            draggable={false}
+                            pauseOnHover={false}
+                            theme="light"
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>)
+    );
+}
+
+export default BookInfo;
--- FrontEnd/src/pages/BookDetails/index.js
+++ FrontEnd/src/pages/BookDetails/index.js
@@ -1,7 +1,6 @@
 import classNames from 'classnames/bind';
 import styles from './BookDetails.module.css';
 import React, { useEffect, useState } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useParams } from 'react-router-dom';
 import ApiService from "../../service/api.service";
@@ -10,18 +9,16 @@ import BookReview from './BookReview';
 import RelatedBook from './RelatedBook';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
-
-
-
+import BookInfo from './BookInfo';
 
 const cx = classNames.bind(styles);
 
 function BookDetails() {
     const { slug } = useParams();
     const [book, setBook] = useState(null);
-    const [quantity, setQuantity] = useState(1);
     const [reviews,setReviews] = useState([]);
     const [averageRating, setAverageRating] = useState(0);
+
     useEffect(() => { 
         async function getBook() {
             try {
@@ -32,10 +29,10 @@ function BookDetails() {
                 window.scrollTo(0, 0);
               }
             } catch (error) {
-              console.error("Error find book:", error);
+                console.error("Error find book:", error);
             }
-          }
-          getBook();
+        }
+        getBook();
     }, [slug]);
     useEffect(() => { 
         async function getBookReview() {
@@ -52,14 +49,7 @@ function BookDetails() {
           }
           getBookReview();
     }, [reviews]);
-    
-    const limitWord = (description, maxLength) => {
-        const data = description.split(' ');
-        if (data.length <= maxLength) {
-          return data.slice(0, data.length).join(' ');
-        }
-        return data.slice(0, maxLength).join(' ') + '...';
-    };
+
     const calculateAverageRating = async(reviews) => {
         if (reviews.length === 0) return 0;
         let totalRating = 0;
@@ -70,65 +60,7 @@ function BookDetails() {
         const averageRating = totalRating / reviews.length
         setAverageRating(averageRating.toFixed(1));
     };
-    const handleChange = (event, maxQuantity) => {
-        let value = event.target.value;
-        if(value > maxQuantity){
-            event.target.value = maxQuantity;
-            value = maxQuantity
-        }
-        else if(!value){
-            value = 1;
-            event.target.value = 1;
-        }
-        setQuantity(value);
-    };
-
-    const handleAddToCart = async (bookId, quantity) => {
-        try{
-            const response = await ApiService.post('carts/add', {
-                bookId,
-                quantity
-            })
-            if(response.status === 200){
-                toast.success('Thêm vào giỏ hàng thành công!', {
-                    position: "top-right",
-                    autoClose: 3000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: false,
-                    draggable: false,
-                    progress: undefined,
-                    theme: "light",
-                });
-                console.log("Successful");
-            }else{
-                toast.error('Thêm vào giỏ hàng thất bại!', {
-                    position: "top-right",
-                    autoClose: 3000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "light",
-                });
-                console.log("Unsuccessful");
-            }
-        }catch(error){
-            toast.error('Thêm vào giỏ hàng thất bại!', {
-                position: "top-right",
-                autoClose: 3000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "light",
-            });
-            console.error('Error add to cart:',error);
-        }
-    }
-
+    
     return ( 
         <>
             { book && (
@@ -136,53 +68,9 @@ function BookDetails() {
                     <div className={cx('container')}>
                         <div className={cx('content-area')}>
                             <div className={cx('wrap-book-item')}>
-                                <div className={cx('details')}>
-                                    <div className={cx('big-img')}>
-                                        <img src={book.avatar} alt='Book img'></img>
-                                    </div>
-                                    <div className={cx('grid')}>
-                                        <div className={cx('book-info')}>
-                                            <h4 className={cx('book-title')}>{book.name}</h4>
-                                            <div className={cx('book-price')}>
-                                                <p className={cx('book-final-price')}>{(book.priceFinal / 1000).toFixed(3)} VNĐ</p>
-                                                <p className={cx('book-original-price')}>{(book.priceOriginal / 1000).toFixed(3)} VNĐ</p>
-                                            </div>
-                                            <div>
-                                                <div className={cx('fw-medium mb-2')}>Tác giả: {book.author.fullName}</div>
-                                                <div className={cx('fw-medium mb-2')}>Dịch giả: {book.translator}</div>
-                                                <div className={cx('fw-medium mb-2')}>Nhà xuất bản: {book.publisher}</div>
-                                            </div>
-                                            <p className={cx('book-desc')}>
-                                                {limitWord(book.description, 50)}
-                                                <a className={cx('read-more-desc')} href="#description-tabs">
-                                                    Read More
-                                                </a>
-                                            </p>
-                                            <div className={cx('remain-quantity')}>
-                                                <span>Còn hàng:&nbsp;</span> 
-                                                <span>{book.quantity} sản phẩm</span>
-                                            </div>
-                                            <div className={cx('cart')}>
-                                                <input className={cx('quantity')} type='number' id='quantity'  aria-label='Product quantity' size='4' min='1' max={book.quantity} step='1' placeholder='' inputMode='numeric' autoComplete='on' defaultValue="1" onChange={(e) => handleChange(e, book.quantity)}></input>
-                                                <button type='button' className={cx('btn')} onClick={() => handleAddToCart(book._id,quantity)}>Add to cart</button>
-                                                <ToastContainer 
-                                                    position="top-right"
-                                                    autoClose={5000}
-                                                    hideProgressBar={false}
-                                                    newestOnTop={false}
-                                                    closeOnClick
-                                                    rtl={false}
-                                                    pauseOnFocusLoss={false}
-                                                    draggable={false}
-                                                    pauseOnHover={false}
-                                                    theme="light"
-                                                />
-                                            </div>
-                                        </div>    
-                                    </div>    
-                                </div>  
+                                <BookInfo bookInfo = {book}/> 
                                 <div className='average-rating'>
-                                        {averageRating}/5 <FontAwesomeIcon icon={faStar} className={cx('yellow-star')}/> ({reviews.length} lượt đánh giá)
+                                    {averageRating}/5 <FontAwesomeIcon icon={faStar} className={cx('yellow-star')}/> ({reviews.length} lượt đánh giá)
                                 </div>  
                             </div>
                             <div className={cx('tab-list-container')}>
